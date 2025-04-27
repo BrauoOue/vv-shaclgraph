fdd1808b28697b73d7b43f7388c3599f0d17a4da
Itonkdong <kostadinoskiviktor@yahoo.com>
2025-04-27 19:19:02 +0200

    Initial commit

---
+++ backend/src/main/java/mk/ukim/finki/mk/backend/Web/TestController.java
@@ -0,0 +1,18 @@
+package mk.ukim.finki.mk.backend.Web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller()
+@RequestMapping("/")
+@CrossOrigin(origins = "http://localhost:3000")
+public class TestController
+{
+
+    @GetMapping(value = "tmp")
+    @ResponseBody
+    public String tmp ()
+    {
+        return "Working";
+    }
+}
