a2c7d930e729c683772fe9757a33308e98608ec9
Gorazd <fi.gorazd@gmail.com>
2025-04-30 21:08:04 +0200

    refactor & rename

--- backend/src/main/java/mk/ukim/finki/mk/backend/Service/ConversionService.java
+++ backend/src/main/java/mk/ukim/finki/mk/backend/Service/ConversionService.java
@@ -3,9 +3,8 @@ package mk.ukim.finki.mk.backend.Service;
 import mk.ukim.finki.mk.backend.Models.DTO.data.RdfDataDto;
 import org.springframework.web.multipart.MultipartFile;
 
-public interface GochService {
+public interface ConversionService {
     RdfDataDto processRdf(MultipartFile rdfFile);
-//    byte[] convertDtoToTurtleFile(RdfDataDto rdfData, String filename);
     byte[] convertDtoToTurtleFile(RdfDataDto rdfData);
 
 }
