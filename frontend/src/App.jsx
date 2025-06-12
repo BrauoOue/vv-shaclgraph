89ec6eb9440759bc8be00cb870fe9643024f9440
Itonkdong <kostadinoskiviktor@yahoo.com>
2025-06-12 17:18:40 +0200

    Add frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <div>
+            <table className="table">
+                <thead>
+                <tr>
+                    <th>Col 1</th>
+                    <th>Col 2</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr className="target">
+                    <td>Data 1</td>
+                    <td>Data 2</td>
+                </tr>
+
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default App
